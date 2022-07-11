17e142be89c2a8ef61c8ab17d60b8d54f3414736
Helga-sov <sandu.helga@gmail.com>
2022-07-11 14:04:57 +0200

    first commit

--- pages/about.js
+++ pages/about.js
@@ -1,27 +1,19 @@
-import Head from "next/head";
-import Image from "next/image";
-import Link from "next/link";
-import Footer from "../components/Footer";
-import Navbar from "../components/Navbar";
-import styles from "../styles/Home.module.css";
-
-export default function Home() {
+const About = () => {
 	return (
 		<div>
-			<h1 className={styles.title}>Homepage</h1>
-			<p className={styles.text}>
+			<h1>About</h1>
+			<p>
 				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ac
 				laoreet arcu. Vestibulum a euismod diam, eget cursus justo. Morbi vel
 				vehicula sem. Morbi id scelerisque orci. Suspendisse et venenatis ante.
 			</p>
-			<p className={styles.text}>
+			<p>
 				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ac
 				laoreet arcu. Vestibulum a euismod diam, eget cursus justo. Morbi vel
 				vehicula sem. Morbi id scelerisque orci. Suspendisse et venenatis ante.
 			</p>
-			<Link href="/ninjas">
-				<a className={styles.btn}>See Ninja Listing</a>
-			</Link>
 		</div>
 	);
-}
+};
+
+export default About;

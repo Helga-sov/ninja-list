5a3b17827369f78446a0b7b6b253746b72c9a7c1
Helga-sov <sandu.helga@gmail.com>
2022-07-11 14:12:14 +0200

    added head and meta data to Homepage and About

--- pages/about.js
+++ pages/about.js
@@ -1,18 +1,28 @@
+import Head from "next/head";
+
 const About = () => {
 	return (
-		<div>
-			<h1>About</h1>
-			<p>
-				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ac
-				laoreet arcu. Vestibulum a euismod diam, eget cursus justo. Morbi vel
-				vehicula sem. Morbi id scelerisque orci. Suspendisse et venenatis ante.
-			</p>
-			<p>
-				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ac
-				laoreet arcu. Vestibulum a euismod diam, eget cursus justo. Morbi vel
-				vehicula sem. Morbi id scelerisque orci. Suspendisse et venenatis ante.
-			</p>
-		</div>
+		<>
+			<Head>
+				<title>Ninja List | About</title>
+				<meta name="keywords" content="ninjas"></meta>
+			</Head>
+			<div>
+				<h1>About</h1>
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ac
+					laoreet arcu. Vestibulum a euismod diam, eget cursus justo. Morbi vel
+					vehicula sem. Morbi id scelerisque orci. Suspendisse et venenatis
+					ante.
+				</p>
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ac
+					laoreet arcu. Vestibulum a euismod diam, eget cursus justo. Morbi vel
+					vehicula sem. Morbi id scelerisque orci. Suspendisse et venenatis
+					ante.
+				</p>
+			</div>
+		</>
 	);
 };
 
--- pages/index.js
+++ pages/index.js
@@ -7,21 +7,29 @@ import styles from "../styles/Home.module.css";
 
 export default function Home() {
 	return (
-		<div>
-			<h1 className={styles.title}>Homepage</h1>
-			<p className={styles.text}>
-				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ac
-				laoreet arcu. Vestibulum a euismod diam, eget cursus justo. Morbi vel
-				vehicula sem. Morbi id scelerisque orci. Suspendisse et venenatis ante.
-			</p>
-			<p className={styles.text}>
-				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ac
-				laoreet arcu. Vestibulum a euismod diam, eget cursus justo. Morbi vel
-				vehicula sem. Morbi id scelerisque orci. Suspendisse et venenatis ante.
-			</p>
-			<Link href="/ninjas">
-				<a className={styles.btn}>See Ninja Listing</a>
-			</Link>
-		</div>
+		<>
+			<Head>
+				<title>Ninja List | Home</title>
+				<meta name="keywords" content="ninjas"></meta>
+			</Head>
+			<div>
+				<h1 className={styles.title}>Homepage</h1>
+				<p className={styles.text}>
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ac
+					laoreet arcu. Vestibulum a euismod diam, eget cursus justo. Morbi vel
+					vehicula sem. Morbi id scelerisque orci. Suspendisse et venenatis
+					ante.
+				</p>
+				<p className={styles.text}>
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ac
+					laoreet arcu. Vestibulum a euismod diam, eget cursus justo. Morbi vel
+					vehicula sem. Morbi id scelerisque orci. Suspendisse et venenatis
+					ante.
+				</p>
+				<Link href="/ninjas">
+					<a className={styles.btn}>See Ninja Listing</a>
+				</Link>
+			</div>
+		</>
 	);
 }
